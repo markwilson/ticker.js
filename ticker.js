42be79e226437abab1122ecd897f644b1b9327fc
Mark Wilson <mwilson@rippleffect.com>
2013-08-17 16:51:26 +0100

    Initial commit

---
+++ ticker.js
@@ -0,0 +1,113 @@
+;(function ($) {
+    $.fn.ticker = function (settings) {
+        var me = this;
+
+        var defaultSettings = {
+            gapWidth: 20,
+            speed: 5
+        };
+
+        settings = $.extend(defaultSettings, settings);
+
+        me.each(function (i, outerElement) {
+            var $outerElement = $(outerElement),
+                totalWidth    = 0,
+                $wrapper      = $('<div></div>'),
+                childCount    = $outerElement.children().length;
+
+            // initialise wrapping element
+            $wrapper.insertBefore($outerElement);
+            $outerElement.appendTo($wrapper);
+
+            // initialise wrapper element styling
+            $wrapper.css({
+                width: $wrapper.innerWidth(),
+                overflow: 'hidden'
+            });
+
+            // initialise inner element float styling
+            $outerElement.children().css({
+                float: 'left',
+                marginRight: settings.gapWidth
+            });
+
+            // duplicate all child elements
+            $outerElement.children().each(function (i, innerElement) {
+                var $innerElement = $(innerElement);
+
+                $innerElement.clone().appendTo($outerElement);
+            });
+
+            // calculate width of all child elements
+            $outerElement.children().each(function (i, innerElement) {
+                var $innerElement = $(innerElement);
+
+                totalWidth += $innerElement.outerWidth() + settings.gapWidth;
+            });
+
+            // initialise outer element styling
+            $outerElement.css({
+                width: totalWidth
+            });
+
+            // detect CSS animation support
+            function detectAnimation()
+            {
+                // taken from https://developer.mozilla.org/en-US/docs/Web/Guide/CSS/Using_CSS_animations/Detecting_CSS_animation_support
+                var elm = $outerElement[0],
+                    animation = false,
+                    animationstring = 'animation',
+                    keyframeprefix = '',
+                    domPrefixes = ['Webkit', 'Moz', 'O', 'ms', 'Khtml'],
+                    pfx  = '';
+
+                if (elm.style.animationName) {
+                    animation = true;
+                }
+
+                if (animation === false) {
+                    for (var prefixIndex = 0; prefixIndex < domPrefixes.length; prefixIndex++) {
+                        if (elm.style[domPrefixes[prefixIndex] + 'AnimationName' ] !== undefined) {
+                            pfx = domPrefixes[prefixIndex];
+                            animationstring = pfx + 'Animation';
+                            keyframeprefix = '-' + pfx.toLowerCase() + '-';
+                            animation = true;
+                            break;
+                        }
+                    }
+                }
+
+                return animation;
+            }
+
+            if (true || !detectAnimation()) {
+                function startAnimation()
+                {
+                    $outerElement.css({
+                        marginLeft: 0
+                    }).animate({
+                        marginLeft: -(totalWidth / 2)
+                    }, settings.speed * childCount * 1000, 'linear', function () {
+                        startAnimation();
+                    });
+                }
+
+                startAnimation();
+            } else {
+                $('<style>@-webkit-keyframes tickerAnimation { from { margin-left:0; } to { margin-left: -' + (totalWidth / 2) + 'px; } }</style>').appendTo('head');
+                $('<style>@-moz-keyframes tickerAnimation { from { margin-left:0; } to { margin-left: -' + (totalWidth / 2) + 'px; } }</style>').appendTo('head');
+                $('<style>@-o-keyframes tickerAnimation { from { margin-left:0; } to { margin-left: -' + (totalWidth / 2) + 'px; } }</style>').appendTo('head');
+                $('<style>@-msie-keyframes tickerAnimation { from { margin-left:0; } to { margin-left: -' + (totalWidth / 2) + 'px; } }</style>').appendTo('head');
+                $('<style>@keyframes tickerAnimation { from { margin-left:0; } to { margin-left: -' + (totalWidth / 2) + 'px; } }</style>').appendTo('head');
+
+                $outerElement.css({
+                    '-webkit-animation': 'tickerAnimation ' + (settings.speed * childCount) + 's infinite',
+                    '-moz-animation': 'tickerAnimation ' + (settings.speed * childCount) + 's infinite',
+                    '-o-animation': 'tickerAnimation ' + (settings.speed * childCount) + 's infinite',
+                    '-msie-animation': 'tickerAnimation ' + (settings.speed * childCount) + 's infinite',
+                    animation: 'tickerAnimation ' + (settings.speed * childCount) + 's infinite'
+                });
+            }
+        });
+    };
+})(jQuery);
